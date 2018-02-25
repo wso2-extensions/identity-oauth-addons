fc2fc728d3931fa1bebbe9d88ec443d087214b75
emsw.bandara <emsw.bandara@gmail.com>
2018-02-25 19:54:35 +0530

    Mutual TLS for OAuth Client Authentication

---
+++ component/org.wso2.carbon.identity.oauth2.token.handler.clientauth.mutualtls/src/main/java/org/wso2/carbon/identity/oauth2/token/handler/clientauth/mutualtls/MutualTLSClientAuthenticator.java
@@ -0,0 +1,205 @@
+/*
+ * Copyright (c) 2018, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ * WSO2 Inc. licenses this file to you under the Apache License,
+ * Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied. See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+package org.wso2.carbon.identity.oauth2.token.handler.clientauth.mutualtls;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.oltu.oauth2.common.OAuth;
+import org.wso2.carbon.identity.oauth.common.OAuth2ErrorCodes;
+import org.wso2.carbon.identity.oauth.common.exception.InvalidOAuthClientException;
+import org.wso2.carbon.identity.oauth2.IdentityOAuth2Exception;
+import org.wso2.carbon.identity.oauth2.bean.OAuthClientAuthnContext;
+import org.wso2.carbon.identity.oauth2.client.authentication.AbstractOAuthClientAuthenticator;
+import org.wso2.carbon.identity.oauth2.client.authentication.OAuthClientAuthnException;
+import org.wso2.carbon.identity.oauth2.token.handler.clientauth.mutualtls.utils.MutualTLSUtil;
+import org.wso2.carbon.identity.oauth2.util.OAuth2Util;
+
+import javax.servlet.http.HttpServletRequest;
+import java.security.NoSuchAlgorithmException;
+import java.security.cert.CertificateEncodingException;
+import java.security.cert.X509Certificate;
+import java.util.List;
+import java.util.Map;
+
+import static org.wso2.carbon.identity.oauth2.token.handler.clientauth.mutualtls.utils.MutualTLSUtil.JAVAX_SERVLET_REQUEST_CERTIFICATE;
+
+/**
+ * This class is responsible for authenticating OAuth clients with Mutual TLS. The client will present
+ * client certificate presented to the authorization server during TLS handshake. As a result of successful
+ * validation of the certificate at web container, the certificate will be available in request attributes. This
+ * authenticator will authenticate the client by matching the certificate presented during handshake against the
+ * certificate registered for the client.
+ */
+public class MutualTLSClientAuthenticator extends AbstractOAuthClientAuthenticator {
+
+    private static Log log = LogFactory.getLog(MutualTLSClientAuthenticator.class);
+
+    /**
+     * @param request                 HttpServletRequest which is the incoming request.
+     * @param bodyParams              Body parameter map of the request.
+     * @param oAuthClientAuthnContext OAuth client authentication context.
+     * @return Whether the authentication is successful or not.
+     * @throws OAuthClientAuthnException
+     */
+    @Override
+    public boolean authenticateClient(HttpServletRequest request, Map<String, List> bodyParams,
+                                      OAuthClientAuthnContext oAuthClientAuthnContext)
+            throws OAuthClientAuthnException {
+
+        // In case if the client ID is not set from canAuthenticate method.
+        if (StringUtils.isEmpty(oAuthClientAuthnContext.getClientId())) {
+            oAuthClientAuthnContext.setClientId(getClientId(request, bodyParams, oAuthClientAuthnContext));
+        }
+
+        try {
+            if (log.isDebugEnabled()) {
+                log.debug("Authenticating client : " + oAuthClientAuthnContext.getClientId() + " with public " +
+                        "certificate.");
+            }
+
+            X509Certificate requestCert;
+            Object certObject = request.getAttribute(JAVAX_SERVLET_REQUEST_CERTIFICATE);
+            if (certObject instanceof X509Certificate[]) {
+                X509Certificate[] cert = (X509Certificate[]) certObject;
+                requestCert = cert[0];
+            } else if (certObject instanceof X509Certificate){
+                requestCert = (X509Certificate) certObject;
+            } else {
+                return false;
+            }
+
+            String tenantDomain = OAuth2Util.getTenantDomainOfOauthApp(oAuthClientAuthnContext.getClientId());
+            X509Certificate registeredCert = (X509Certificate) OAuth2Util
+                    .getX509CertOfOAuthApp(oAuthClientAuthnContext.getClientId(), tenantDomain);
+            return authenticate(registeredCert, requestCert);
+
+
+        } catch (IdentityOAuth2Exception e) {
+            throw new OAuthClientAuthnException(OAuth2ErrorCodes.SERVER_ERROR, "Error occurred while retrieving " +
+                    "public certificate of client ID: " + oAuthClientAuthnContext.getClientId(), e);
+        } catch (InvalidOAuthClientException e) {
+            throw new OAuthClientAuthnException(OAuth2ErrorCodes.INVALID_CLIENT, "Error occurred while retrieving " +
+                    "tenant domain for the client ID: " + oAuthClientAuthnContext.getClientId(), e);
+        }
+    }
+
+    /**
+     * Returns whether the incoming request can be authenticated or not using the given inputs.
+     *
+     * @param request    HttpServletRequest which is the incoming request.
+     * @param bodyParams Body parameters present in the request.
+     * @param context    OAuth2 client authentication context.
+     * @return Whether client can be authenticated using this authenticator.
+     */
+    @Override
+    public boolean canAuthenticate(HttpServletRequest request, Map<String, List> bodyParams,
+                                   OAuthClientAuthnContext context) {
+
+        if (clientIdExistsAsParam(bodyParams) && validCertExistsAsAttribute(request)) {
+            if (log.isDebugEnabled()) {
+                log.debug("Client ID exists in request body parameters and a valid certificate found in request " +
+                        "attributes. Hence returning true.");
+            }
+            return true;
+        } else {
+            if (log.isDebugEnabled()) {
+                log.debug("Client id is not available in body params or valid certificate not found in request " +
+                        "attributes. Hence returning false.");
+            }
+            return false;
+        }
+    }
+
+    /**
+     * Retrieves the client ID which is extracted from incoming request.
+     *
+     * @param request                 HttpServletRequest.
+     * @param bodyParams              Body parameter map of the incoming request.
+     * @param oAuthClientAuthnContext OAuthClientAuthentication context.
+     * @return Client ID of the OAuth2 client.
+     * @throws OAuthClientAuthnException OAuth client authentication Exception.
+     */
+    @Override
+    public String getClientId(HttpServletRequest request, Map<String, List> bodyParams, OAuthClientAuthnContext
+            oAuthClientAuthnContext) throws OAuthClientAuthnException {
+
+        Map<String, String> stringContent = getBodyParameters(bodyParams);
+        oAuthClientAuthnContext.setClientId(stringContent.get(OAuth.OAUTH_CLIENT_ID));
+        return oAuthClientAuthnContext.getClientId();
+    }
+
+    private boolean clientIdExistsAsParam(Map<String, List> contentParam) {
+
+        Map<String, String> stringContent = getBodyParameters(contentParam);
+        return (StringUtils.isNotEmpty(stringContent.get(OAuth.OAUTH_CLIENT_ID)));
+    }
+
+    /**
+     * Check for the existence of a valid certificate in required format in the request attribute map.
+     *
+     * @param request HttpServletRequest which is the incoming request.
+     * @return Whether a certificate exists or not.
+     */
+    private boolean validCertExistsAsAttribute(HttpServletRequest request) {
+
+        Object certObject = request.getAttribute(JAVAX_SERVLET_REQUEST_CERTIFICATE);
+        return (certObject instanceof X509Certificate[] || certObject instanceof X509Certificate);
+    }
+
+    /**
+     * Authenticate the client by comparing the public key of the registered public certificate against the public
+     * key of the certificate presented at TLS hand shake for authentication.
+     *
+     * @param registeredCert X.509 certificate registered at service provider configuration.
+     * @param requestCert    X.509 certificate presented to server during TLS hand shake.
+     * @return Whether the client was successfully authenticated or not.
+     */
+    protected boolean authenticate(X509Certificate registeredCert, X509Certificate requestCert)
+            throws OAuthClientAuthnException {
+
+        boolean trustedCert = false;
+        try {
+            String publicKeyOfRegisteredCert = MutualTLSUtil.getThumbPrint(registeredCert);
+            String publicKeyOfRequestCert = MutualTLSUtil.getThumbPrint(requestCert);
+            if (StringUtils.equals(publicKeyOfRegisteredCert, publicKeyOfRequestCert)) {
+                if (log.isDebugEnabled()) {
+                    log.debug("Client certificate thumbprint matched with the registered certificate thumbprint.");
+                }
+                trustedCert = true;
+            }
+        } catch (NoSuchAlgorithmException | CertificateEncodingException e) {
+            throw new OAuthClientAuthnException(OAuth2ErrorCodes.INVALID_GRANT, "Error occurred while " +
+                    "generating certificate thumbprint. Error: " + e.getMessage(), e);
+        }
+        return trustedCert;
+    }
+
+    @Override
+    public int getPriority() {
+
+        return 158;
+    }
+
+    @Override
+    public String getName() {
+
+        return this.getClass().getSimpleName();
+    }
+}
